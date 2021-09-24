71f83c732b528f466d19987cd2c1565f74b20a65
Nhat Nguyen <nhat.eric.nguyen@gmail.com>
2021-09-24 23:29:28 +0700

    retest deploy react

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewsPage from './NewsPage';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 
+import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { createTodo as createPostMutation } from '../graphql/mutations';
+
 import './styles.scss'
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
@@ -15,20 +34,74 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const initialFormState = { title: '', description: '', userName: '', name: '' };
 
 const MainPage = () => {
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [formData, setFormData] = useState(initialFormState);
+  const [posts, setPosts] = useState([]);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setTitle('');
+    setDescription('');
+    setOpen(false);
+  }
+
+  const createNote = async () => {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createPostMutation, variables: { input: formData } });
+    setPosts([...posts, formData]);
+    setFormData(initialFormState);
+  }
+
+  const handleCreate = () => {
+    if (title && description) {
+      console.log('title', title);
+      console.log('description', description);
+
+    }
+  }
+
   return (<div>
     <div className='news-container'>
       <NewsPage />
     </div>
     <Grid container spacing={2}>
       <Grid item xs={6}>
-        <Item><Button variant="contained" style={{ width: '100%', height: '50px' }}>Create a post</Button></Item>
+        <Item><Button variant="contained" onClick={handleOpen} style={{ width: '100%', height: '50px' }}>Create a post</Button></Item>
       </Grid>
       <Grid item xs={6}>
         <Item><AmplifySignOut /></Item>
       </Grid>
     </Grid>
+
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <TextField
+          id="standard-basic"
+          label="Input Title"
+          variant="standard"
+          onChange={(e) => setTitle(e.target.value)}
+          sx={{ width: '100%', marginBottom: '16px' }}
+        />
+        <TextareaAutosize
+          aria-label="minimum height"
+          minRows={3}
+          placeholder="Minimum 3 rows"
+          style={{ width: '100%', marginBottom: '16px' }}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <Button variant="contained" onClick={handleCreate}>Create</Button>
+      </Box>
+    </Modal>
   </div>)
 };
 
